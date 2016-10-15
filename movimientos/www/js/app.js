004ff4bfa34f15708268f4ae04be50a4eacccd22
fepe15 <fedegperalta@gmail.com>
2016-10-15 05:54:19 -0300

    funcionando

--- movimientos/www/js/app.js
+++ movimientos/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -65,4 +65,39 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
-});
+})
+
+
+.directive('onLongPress', function($timeout) {
+  return {
+    restrict: 'A',
+    link: function($scope, $elm, $attrs) {
+      $elm.bind('touchstart', function(evt) {
+        // Locally scoped variable that will keep track of the long press
+        $scope.longPress = true;
+
+        // We'll set a timeout for 600 ms for a long press
+        $timeout(function() {
+          if ($scope.longPress) {
+            // If the touchend event hasn't fired,
+            // apply the function given in on the element's on-long-press attribute
+            $scope.$apply(function() {
+              $scope.$eval($attrs.onLongPress)
+            });
+          }
+        }, 600);
+      });
+
+      $elm.bind('touchend', function(evt) {
+        // Prevent the onLongPress event from firing
+        $scope.longPress = false;
+        // If there is an on-touch-end function attached to this element, apply it
+        if ($attrs.onTouchEnd) {
+          $scope.$apply(function() {
+            $scope.$eval($attrs.onTouchEnd)
+          });
+        }
+      });
+    }
+  };
+})
--- movimientos/www/js/controllers.js
+++ movimientos/www/js/controllers.js
@@ -1,8 +1,229 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicPlatform, $cordovaMedia, $timeout, $cordovaDeviceMotion) {
+
+  $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 1};
+
+  $scope.Ingresar = false;
+  $scope.Empezar = function(){
+    $scope.Ingresar = true;
+  }
+
+  $ionicPlatform.ready(function() {
+    
+    var options = { frequency: 1000 };
+
+        var watch = $cordovaDeviceMotion.watchAcceleration(options);
+        watch.then(
+          null,
+          function(error) {
+          console.log("No se puede tomar coordenadas desde ordenador");
+          },
+          function(result) {
+            $scope.X = parseInt(result.x);
+            $scope.Y = parseInt(result.y);
+            $scope.Z = parseInt(result.z);
+            $scope.tiempo = result.timestamp;
+
+            if($scope.X > 4 && $scope.audioIzq != null && $scope.arrayPlay["Izquierda"] == 1){
+
+              $scope.audioIzq.play();
+              $scope.arrayPlay = {Izquierda: 0, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 1};
+
+            }
+
+            if($scope.X < -4 && $scope.audioDer != null && $scope.arrayPlay["Derecha"] == 1){
+
+              $scope.audioDer.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 0, Abajo: 1, Arriba: 1, Acostado: 1};
+
+            }
+
+            if($scope.Y > 5 && $scope.audioArriba != null && $scope.arrayPlay["Arriba"] == 1){
+
+              $scope.audioArriba.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 0, Acostado: 1};
+
+            }
+
+            if($scope.Z > 8 && $scope.audioAcostado != null && $scope.arrayPlay["Acostado"] == 1){
+
+              $scope.audioAcostado.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 0};
+
+            }
+
+            if($scope.Z < -7 && $scope.audioAbajo != null && $scope.arrayPlay["Abajo"] == 1){
+
+              $scope.audioAbajo.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 0, Arriba: 1, Acostado: 1};
+
+            }
+
+
+        });
+
+    })
+
+
+
+  $scope.AudioIzquierda = function(){
+
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "izquierda.mp3";
+        $scope.audioIzq = $cordovaMedia.newMedia(src);
+        $scope.audioIzq.startRecord();
+        //var detalles = JSON.stringify($scope.audioIzq);
+        //alert(detalles);
+
+        /*$timeout(function(){
+          $scope.audioIzq.stopRecord();
+          alert("grabado!!!");
+        }, 2000)*/
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+  $scope.Detener = function(btn){
+    try{
+
+      if(btn == 'btnIzquierda'){
+
+        $scope.audioIzq.stopRecord();
+
+      }
+
+      if(btn == 'btnDerecha'){
+
+        $scope.audioDer.stopRecord();
+
+      }
+
+      if(btn == 'btnAbajo'){
+
+        $scope.audioAbajo.stopRecord();
+
+      }
+
+      if(btn == 'btnArriba'){
+
+        $scope.audioArriba.stopRecord();
+
+      }
+
+      if(btn == 'btnAcostado'){
+
+        $scope.audioAcostado.stopRecord();
+
+      }
+      
+
+    } catch(ex){
+      alert(ex);
+    }
+  }
+
+  $scope.AudioDerecha = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "derecha.mp3";
+        $scope.audioDer = $cordovaMedia.newMedia(src);
+        $scope.audioDer.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+
+  $scope.AudioAbajo = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "abajo.mp3";
+        $scope.audioAbajo = $cordovaMedia.newMedia(src);
+        $scope.audioAbajo.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+  $scope.AudioArriba = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "arriba.mp3";
+        $scope.audioArriba = $cordovaMedia.newMedia(src);
+        $scope.audioArriba.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+  $scope.AudioAcostado = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "acostado.mp3";
+        $scope.audioAcostado = $cordovaMedia.newMedia(src);
+        $scope.audioAcostado.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+
 })
 
-.controller('AutorCtrl', function($scope) {
+.controller('AutorCtrl', function($scope, $ionicPlatform, $cordovaInAppBrowser) {
   
+  var options = {
+      location: 'yes',
+      clearcache: 'yes',
+      toolbar: 'no'
+    };
+
+  $scope.VerGitHub=function(){
+    $ionicPlatform.ready(function(){
+
+      $cordovaInAppBrowser.open('https://github.com/jmgiusti91', '_system', options);
+
+    });
+    
+  }
 })
